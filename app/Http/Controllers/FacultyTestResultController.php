6c8e2342c6c6f6146711635e124238db2b9cdd7a
Mirvali <leemirvali@gmail.com>
2021-02-02 17:52:29 +0500

    New models cteated

---
+++ app/Http/Controllers/FacultyTestResultController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\FacultyTestResult;
+use Illuminate\Http\Request;
+
+class FacultyTestResultController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\FacultyTestResult  $facultyTestResult
+     * @return \Illuminate\Http\Response
+     */
+    public function show(FacultyTestResult $facultyTestResult)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\FacultyTestResult  $facultyTestResult
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(FacultyTestResult $facultyTestResult)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\FacultyTestResult  $facultyTestResult
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, FacultyTestResult $facultyTestResult)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\FacultyTestResult  $facultyTestResult
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(FacultyTestResult $facultyTestResult)
+    {
+        //
+    }
+}
