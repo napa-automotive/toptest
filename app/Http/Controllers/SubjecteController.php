1b1379bf0c81be446e45bc9df8113db6345cf284
Nurxoniddin <nurxoniddin@gmail.com>
2021-02-03 18:06:52 +0500

    First commit

---
+++ app/Http/Controllers/SubjecteController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Subjecte;
+use Illuminate\Http\Request;
+
+class SubjecteController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Subjecte  $subjecte
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Subjecte $subjecte)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Subjecte  $subjecte
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Subjecte $subjecte)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Subjecte  $subjecte
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Subjecte $subjecte)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Subjecte  $subjecte
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Subjecte $subjecte)
+    {
+        //
+    }
+}
---
+++ database/factories/SubjecteFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Subjecte;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class SubjecteFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Subjecte::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
