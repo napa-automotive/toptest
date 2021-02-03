414754fc898e2b813e401d606e061dff9f07a36a
Mirvali <leemirvali@gmail.com>
2021-02-03 13:10:07 +0500

    seeder models created

---
+++ database/factories/SubfieldFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Subfield;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class SubfieldFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Subfield::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
---
+++ database/factories/UserSessionFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\UserSession;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class UserSessionFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = UserSession::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
