eae972360f67a027a55da7461b3095064e820c2f
Mirvali97 <leemirvali@gmail.com>
2021-02-04 01:18:24 +0500

    Loop

---
+++ app/Http/Controllers/LoopController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Loop;
+use Illuminate\Http\Request;
+
+class LoopController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Loop  $loop
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Loop $loop)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Loop  $loop
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Loop $loop)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Loop  $loop
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Loop $loop)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Loop  $loop
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Loop $loop)
+    {
+        //
+    }
+}
---
+++ database/factories/LoopFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Loop;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class LoopFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Loop::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
