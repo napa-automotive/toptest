42fad6460c70792e6d0c0164e37dd02b8123cda0
mirvali97 <leemirvali@gmail.com>
2021-02-04 04:03:38 +0500

    model created -a

---
+++ app/Http/Controllers/UserTestAnswerController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\UserTestAnswer;
+use Illuminate\Http\Request;
+
+class UserTestAnswerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\UserTestAnswer  $userTestAnswer
+     * @return \Illuminate\Http\Response
+     */
+    public function show(UserTestAnswer $userTestAnswer)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\UserTestAnswer  $userTestAnswer
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(UserTestAnswer $userTestAnswer)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\UserTestAnswer  $userTestAnswer
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, UserTestAnswer $userTestAnswer)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\UserTestAnswer  $userTestAnswer
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(UserTestAnswer $userTestAnswer)
+    {
+        //
+    }
+}
--- database/factories/UserTestFactory.php
+++ database/factories/UserTestFactory.php
@@ -2,17 +2,17 @@
 
 namespace Database\Factories;
 
-use App\Models\User;
+use App\Models\UserTest;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-class UserFactory extends Factory
+class UserTestFactory extends Factory
 {
     /**
      * The name of the factory's corresponding model.
      *
      * @var string
      */
-    protected $model = User::class;
+    protected $model = UserTest::class;
 
     /**
      * Define the model's default state.
