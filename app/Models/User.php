a6c992790c912f6fd325dab981d47736aff6324d
Mirvali <leemirvali@gmail.com>
2021-02-27 22:23:08 +0500

    for checking model, How to switch From current model to own created model

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,25 +2,65 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Orchid\Platform\Models\User as Authenticatable;
 
-class User extends Model
+class User extends Authenticatable
 {
-    use HasFactory;
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'permissions',
+    ];
 
-    public function userTests()
-    {
-        return $this->hasMany(UserTest::class);
-    }
+    /**
+     * The attributes excluded from the model's JSON form.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+        'permissions',
+    ];
 
-    public function userExams()
-    {
-        return $this->hasMany(UserExam::class);
-    }
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'permissions'          => 'array',
+        'email_verified_at'    => 'datetime',
+    ];
 
-    public function userSettings()
-    {
-        return $this->hasMany(UserSetting::class);
-    }
+    /**
+     * The attributes for which you can use filters in url.
+     *
+     * @var array
+     */
+    protected $allowedFilters = [
+        'id',
+        'name',
+        'email',
+        'permissions',
+    ];
+
+    /**
+     * The attributes for which can use sort in url.
+     *
+     * @var array
+     */
+    protected $allowedSorts = [
+        'id',
+        'name',
+        'email',
+        'updated_at',
+        'created_at',
+    ];
 }
